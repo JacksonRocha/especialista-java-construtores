1000c24c9986fe876604216c635d87914baee1a1
jacksonrocha <jackson_lbr@hotmail.com>
2024-09-02 22:04:22 -0300

    Tipos de parametros

--- src/main/java/jackson/rocha/Principal.java
+++ src/main/java/jackson/rocha/Principal.java
@@ -2,8 +2,16 @@ package jackson.rocha;
 
 public class Principal {
     public static void main(String[] args) {
-        Produto produto1 = new Produto();
-        Produto produto2 = new Produto();
+        Produto produto1 = new Produto("Picanha");
+        System.out.println(produto1.nome);
+        System.out.println(produto1.quantidadeEstoque);
 
+        Produto produto2 = new Produto("Coxão mole", 50);
+        System.out.println(produto2.nome);
+        System.out.println(produto2.quantidadeEstoque);
+
+        Produto produto3 = new Produto();
+        System.out.println(produto3.nome);
+        System.out.println(produto3.quantidadeEstoque);
     }
 }
--- src/main/java/jackson/rocha/Produto.java
+++ src/main/java/jackson/rocha/Produto.java
@@ -2,11 +2,24 @@ package jackson.rocha;
 
 public class Produto {
 
+    private static final int QUANTIDADE_ESTOQUE_INICIAL = 100;
+
+    public String nome;
     int quantidadeEstoque;
 
     Produto() {
-        this.quantidadeEstoque = 10;
-        System.out.println("Construindo um produto");
+        this.nome = "Sem nome";
+        this.quantidadeEstoque = QUANTIDADE_ESTOQUE_INICIAL;
+    }
+
+    Produto(String nome) {
+        this.nome = nome;
+        this.quantidadeEstoque = QUANTIDADE_ESTOQUE_INICIAL;
+    }
+
+    Produto(String nome, int estoqueInicial) {
+        this.nome = nome;
+        this.quantidadeEstoque = estoqueInicial;
     }
 
 }
